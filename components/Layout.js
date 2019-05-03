b3465287dbb7addcb8b01e8fdd14e639cac8b968
Carl Hedgren <carl.hedgren@creuna.no>
2019-05-03 14:30:34 +0200

    poc af

--- components/Layout.js
+++ components/Layout.js
@@ -2,33 +2,22 @@ import Head from 'next/head'
 import Link from 'next/link'
 import globalStyles from './styles/global'
 import styles from './styles/layout'
-import GithubCorner from './GithubCorner'
 
 export default props => (
   <div>
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Sanity + Next.js = 💖</title>
+      <title>Sanity + Creuna Poc</title>
     </Head>
     <nav>
       <Link href="/movies"><a>Movies</a></Link>
       <Link href="/people"><a>People</a></Link>
     </nav>
-    <GithubCorner />
     <div id="main">
       {props.children}
     </div>
     <footer>
-      <a href="http://sanity.io">
-        <img src="https://rawgit.com/sanity-io/example-frontend-next-js/master/static/sanity-logo.svg"/>
-      </a>
-      +
-      <a href="https://github.com/zeit/next.js">
-        <img className="next" src="https://cloud.githubusercontent.com/assets/13041/19686250/971bf7f8-9ac0-11e6-975c-188defd82df1.png"/>
-      </a>
-      =
-      💖
     </footer>
     <style jsx>{styles}</style>
     <style jsx global>{globalStyles}</style>
--- pages/movies.js
+++ pages/movies.js
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 import sanity from '../lib/sanity'
 import styles from './styles/movies'
-import listStyles from './styles/list'
 import sanityClient from '../lib/sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
@@ -13,14 +12,7 @@ function imageUrlFor(source) {
   return imageBuilder.image(source)
 }
 
-const query = `*[_type == "movie"] {
-  _id,
-  title,
-  releaseDate,
-  poster,
-  "posterAspect": poster.asset->.metadata.dimensions.aspectRatio,
-  "director": crewMembers[job == "Director"][0].person->name
-}[0...50]
+const query = `*[_type == "person"] {name}[0...50]
 `
 
 export default class Movies extends React.Component {
@@ -38,31 +30,11 @@ export default class Movies extends React.Component {
         <div className="movies">
           <ul className="list">
             {movies.map(movie => (
-              <li key={movie._id} className="list__item">
-                <Link href={{pathname: '/movie', query: {id: movie._id}}}>
-                  <a>
-                    {movie.poster && (
-                      <img
-                        src={imageUrlFor(movie.poster).ignoreImageParams().width(300)}
-                        width="100"
-                        height={100 / movie.posterAspect}
-                      />
-                    )}
-                    <div style={{paddingTop: '0.2em'}}>{movie.releaseDate.substr(0, 4)}</div>
-                    <h3>{movie.title}</h3>
-                    {movie.director && (
-                      <span className="movies-list__directed-by">
-                        Directed by {movie.director}
-                      </span>
-                    )}
-                  </a>
-                </Link>
-              </li>
-            ))}
+                    <h1> {movie.name} </h1>)
+              )}
           </ul>
         </div>
         <style jsx>{styles}</style>
-        <style jsx>{listStyles}</style>
       </Layout>
     )
   }

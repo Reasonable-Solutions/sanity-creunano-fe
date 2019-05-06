e7722bfd8667bf13647b09ade8fa5ca08ade5b68
Carl Hedgren <carl.hedgren@creuna.no>
2019-05-06 16:24:52 +0200

    adjust styles

--- components/styles/header.js
+++ components/styles/header.js
@@ -5,11 +5,12 @@ export default css`
 nav {
   display: flex;
   justify-content: space-around;
+  transform: rotate(3.4deg) translateY(calc(-40px - 1.5vw));
 }
 
 header {
   display: flex;
-  flex-direction: column;
+  flex-direction: column-reverse;
   filter: invert(100%);
   min-height: 300px;
   height: 50vh;

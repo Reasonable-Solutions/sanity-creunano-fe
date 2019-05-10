4803d889deaa33d12f369184ad4889632623c1f2
Carl Hedgren <carl.hedgren@creuna.no>
2019-05-10 10:36:34 +0200

    remove avenir

--- components/styles/global.js
+++ components/styles/global.js
@@ -4,5 +4,5 @@ export default css`
 * { box-sizing: border-box; }
 body {
   margin: 0;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Verdana, Helvetica, Arial, sans-serif;
 }`

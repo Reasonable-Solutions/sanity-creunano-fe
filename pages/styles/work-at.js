979bf66fa81bacdd0808bc391a6efcba3dd5698d
Lars K <li.kleven@creuna.no>
2019-05-13 14:25:20 +0200

    add responsivity to work-at page / components

--- pages/styles/work-at.js
+++ pages/styles/work-at.js
@@ -1,6 +1,27 @@
-import css from 'styled-jsx/css'
+import css from "styled-jsx/css";
+import { breakpoints } from "./breakpoints.js";
 
 export default css`
-li { list-style: none; width: 50%; padding: 20px; }
-ul { display: flex; flex-wrap: wrap; padding: 20px;}
-`
+  li {
+    list-style: none;
+    width: 100%;
+  }
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 0;
+  }
+
+  @media screen and ${breakpoints.small} {
+    ul {
+      flex-direction: row;
+      padding: 20px;
+    }
+    li {
+      width: 50%;
+      display: flex;
+    }
+  }
+`;

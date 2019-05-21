15967f44a080ffc904af31f484f67f157e7fbb80
Lars K <li.kleven@creuna.no>
2019-05-21 13:34:04 +0200

    use Hero in preview.js

--- pages/preview.js
+++ pages/preview.js
@@ -3,10 +3,14 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 import { sanityPreviewClient, queries } from "../lib/sanity";
 import BlockContent from "@sanity/block-content-to-react";
-import sanityClient from "../lib/sanity";
+import sanity from "../lib/sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import { Jobad } from "./work-ad";
 import PersonCard from "../components/personCard";
+import Hero from "../components/HeroContent";
+
+let builder = imageUrlBuilder(sanity);
+let mkUrl = source => builder.image(source);
 
 export default class Preview extends React.Component {
   static async getInitialProps(req) {
@@ -38,7 +42,12 @@ export default class Preview extends React.Component {
 let previewMatch = (type, preview) => {
   switch (type) {
     case "jobAd":
-      return <Jobad {...preview} />;
+      return (
+        <>
+          <Hero {...preview} mkUrl={mkUrl} />
+          <Jobad {...preview} />
+        </>
+      );
     case "person":
       return <PersonCard {...preview} />;
   }

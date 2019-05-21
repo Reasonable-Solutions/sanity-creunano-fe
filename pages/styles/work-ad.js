545875f45d97c813b14a0f6907b7c6278db4c817
Lars K <li.kleven@creuna.no>
2019-05-21 09:19:54 +0200

    removed hero styles from work-ad.js styles

--- pages/styles/work-ad.js
+++ pages/styles/work-ad.js
@@ -2,7 +2,9 @@ import css from "styled-jsx/css";
 import { breakpoints } from "../../pages/styles/breakpoints.js";
 
 export default css`
-  .main-text { padding-right: 80px; }
+  .main-text {
+    padding-right: 80px;
+  }
   .jobAd {
     display: flex;
     flex-direction: column;
@@ -15,47 +17,4 @@ export default css`
       flex-direction: row;
     }
   }
-
-  .hero-wrapper {
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
-  }
-  .hero {
-    position: relative;
-    display: -ms-flexbox;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    will-change: opacity;
-    min-height: 80vh;
-    color: white;
-    text-decoration: none;
-    background: #000;
-    z-index: 1;
-  }
-
-.hero-image { object-fit: cover; width: 100%;}
-.hero-media {
-        display: flex;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	overflow: hidden;
-        opacity: 0.5;
-        width: inherit;
-        min-height: 100%;
-        object-fit: cover;
-}
-
-.hero-content {
-	width: 100%;
-	max-width: 1400px;
-	margin: 0 auto;
-	padding: 20px 30px;
-	position: relative;
-	z-index: 3;
-}
-`
+`;
